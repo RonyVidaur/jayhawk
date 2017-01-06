778f14469644d511379b98c11a62a1d3481a9893
RonyVidaur <rony.vidaur@gmail.com>
2017-01-06 17:32:14 -0600

    basic styles defined, test running on app/ components needed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import logo from './assets/images/logo.svg'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {mounted: false}
+  }
   render() {
     return (
       <div className="App">
-        <h1>JAYHAWK</h1>
-      </div>
-    );
+
+        <img src={logo} alt="the project jayhawk logo" className="logo"/>
+        <a className="button">Are</a>
+    </div>
+    )
   }
 }
 
-export default App;
+export default App
