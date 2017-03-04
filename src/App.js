587c70121c18a2edce371058eefdcf27c65c6fe8
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-04 10:52:51 -0600

    total project reboot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import Holder from './components/Holder'
+
 
 class App extends Component {
   render() {
     return (
     <div className="App">
-      <Holder />
+
     </div>
     )
   }
--- src/components/common/Logo.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const Logo = styled.img`
-  hight: ${ props => props.mini ? '60px' : '100px' };
-  width: ${ props => props.mini ? '60px' : '100px'};
-`
-
-export default Logo
