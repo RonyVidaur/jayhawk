da6149f1d9b31eb5bb5242efe9874a281146c73e
Rony Vidaur <rony.vidaur@gmail.com>
2017-03-05 23:06:12 -0600

    set dashboard as home screen [DEV]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import './App.css'
-
-
+import Dashboard from './components/dashboard/dashboard'
 class App extends Component {
   render() {
     return (
-    <div className="App">
-
-    </div>
-    )
+     <Dashboard />
+  )
   }
 }
 
